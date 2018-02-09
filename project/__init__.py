bb8b632f4b159f3ae6beb520e37767a09c01e09b
razenmarling <razen.marling3@gmail.com>
2018-02-10 03:44:40 +0800

    logout

--- project/__init__.py
+++ project/__init__.py
@@ -20,25 +20,41 @@ def make_session_permanent():
 def index():
 	errormessage = ''
 
+	if any([session.get('admin_logged'), session.get('user_logged')]):
+		return redirect(url_for('user_dashboard'))
+
 	if request.method == 'POST':
 		username = request.form['username']
 		password = request.form['password']
 
 		if username == "admin" and password == "admin":
 			session["admin_logged"] = True
+			session['user'] = 'admin'
 			return redirect(url_for('admin_dashboard'))
 		elif username == "user" and password == "user":
 			session["user_logged"] = True
+			session['user'] = 'user'
 			return redirect(url_for('user_dashboard'))
 		else:
 			errormessage = 'Invalid username or password'
 
 	return render_template('login.html', errormessage=errormessage)
 
+@app.route('/logout/<type>')
+def logout(type):
+	if type == 'admin':
+		session.pop('admin_logged')
+	elif type == 'user':
+		session.pop('user_logged')
+
+	session.pop('user')
+
+	return redirect(url_for('index'))
+
 @app.route('/admin/dashboard', methods=['GET'])
 def admin_dashboard():
 	if session.get('admin_logged', None):
-		return render_template('admin_dashboard.html')
+		return render_template('admin_dashboard.html', user=session['user'])
 	else:
 		return redirect(url_for('index'))
 
